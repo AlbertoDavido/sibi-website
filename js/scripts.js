d0ba27473ebf5769a423df2265bcfee8dc13949e
David Wamenju <wamenjudavid@gmail.com>
2025-06-26 10:58:02 +0300

    check if api affects runtime

---
+++ js/scripts.js
@@ -0,0 +1,42 @@
+const displayValue=document.querySelectorAll(".num");
+let hasAnimated=false;
+
+let interval=5000;
+
+displayValue.forEach(value => {
+    let startValue=0;
+    let endValue=parseInt(value.getAttribute("data-val"));
+
+    let duration=Math.floor(interval/endValue);
+    let counter=setInterval(function(){
+        startValue+=1;
+        value.textContent=startValue + "+";
+        if(startValue==endValue){
+            clearInterval(counter);
+        }
+    }, duration);
+});
+const scroll=document.getElementById("partner-scroll");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
